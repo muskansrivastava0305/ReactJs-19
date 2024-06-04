d60a5946ec338110da3f97b569edde1280e9b685
Muskan Kumari <muskansrivastava0305@gmail.com>
2024-06-04 20:22:43 +0530

    Day 05(1-project)

---
+++ Project1-NetflixCard/src/App.jsx
@@ -0,0 +1,14 @@
+export const App = () =>{
+  return (
+  <div>
+  <div>
+    <img src="BP.jpg" alt="BP.jpg" width="30%" height="30%"/>
+  </div>
+   <h2>Name: Business Proposal </h2>
+  <h3> Rating:8.1</h3>
+  <p>A young woman pretends to be her friend and goes on her blind date in her place, only to discover that the date is her CEO. 
+    She must hide her true identity from him while he pursues her and insists they get married.</p>
+    </div>
+  )
+}
+
